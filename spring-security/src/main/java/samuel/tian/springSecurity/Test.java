9d995f31a89fad31fd57d621a1d33c8460fccb61
SamTianGit <tiansiyu_ecust@hotmail.com>
2015-08-13 15:03:23 -0400

    added authentication object to main thread

--- spring-security/src/main/java/samuel/tian/springSecurity/Test.java
+++ spring-security/src/main/java/samuel/tian/springSecurity/Test.java
@@ -2,14 +2,21 @@ package samuel.tian.springSecurity;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class Test {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("securityContext.xml");
+		SecurityContext sc = SecurityContextHolder.getContext();
+		Authentication au = new UsernamePasswordAuthenticationToken("sam","sam");
+		sc.setAuthentication(au);
 		MyClass mc = (MyClass) ac.getBean("myClass");
-		mc.executeNormalSection();
 		mc.executeCriticalSection();
+		mc.executeNormalSection();
 	}
 
 }
